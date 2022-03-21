d97a0fc7927db20663472a7eb4591a3bcc831724
Sandra Thomas <sandramt1501@gmail.com>
2022-03-21 08:27:17 +0100

    First Commit done

---
+++ storage/framework/views/17f64d28ccee6db88898d8d9fe5259389b0d0ae6.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" value="<?php echo e(csrf_token()); ?>" />
+    <title>Vue JS CRUD Operations in Laravel</title>
+    <link href="<?php echo e(mix('css/app.css')); ?>" type="text/css" rel="stylesheet" />
+</head>
+<body>
+    <div id="app"></div>
+    <script src="<?php echo e(mix('js/app.js')); ?>" type="text/javascript"></script>
+</body>
+</html><?php /**PATH /Users/sandramerinthomas/Desktop/Tourispo/laravel-vue-crud/resources/views/layout/app.blade.php ENDPATH**/ ?>
